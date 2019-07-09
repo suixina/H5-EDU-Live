7eb3259221b437a61660029c17905ece68e68993
suixina <suixinsd@163.com>
2019-07-09 22:08:15 +0800

    sxcvhfghk

--- src/ELearning/assets/js/communityLode.js
+++ src/ELearning/assets/js/communityLode.js
@@ -4,65 +4,57 @@
     $(".indexTable_con").hide().eq(indexs).show();
 });*/
 function queryTitle(){
-    var search=$("#search");
+    let search=$("#search").val();
     $.ajax({
         type:"post",
         url : "/community/summaries",/*+postId + "/getPost"*/
         dataType:"json",
-        data:"search",
+        data:{"search":search},
         success:function succ(postList) {
-            for(let i=0;i<postList.length;i++){
-                if(postList.length>0){
-                    $("#postDiv").append("<div class='indexTable_son' style='display: block'>"+postList[i].postId
-                        +"<div class='inHeadPic'><img src="+postList[i].headPic+"alt=''></div>"
-                        +"<a class=\"indexTable_sonHead\" href='ta.html'>"+postList[i].author+"</a>"+"</div>"
-                        +"<div class=\"indexTableCon\">"
-                        +"<h1><a href='/lt_content?postId="+postList[i].postId+"'></a>"+postList[i].title+"</h1>"
-                        // +"<p>"+postList[i].mBody+"</p>"
-                        +"<ul class=\"indexTable_conFoot clearfix\">"
-                        +"<li>"+postList[i].pubTime+"</li>"
-                        +"<li><a href=\"lt_content.html\">评论</a>"+postList[i].heat+"</li>"
-                        +"<li>最新回复时间："+postList[i].latestTime+"</li>"
-                        +"</ul>"
-                        +"</div>");
+            for(let users in postList) {
+                $("#postDiv").append("<div class='indexTable_son' style='display: block'>" + users.postId
+                    + "<div class='inHeadPic'><img src=" + users.headPic + "alt=''></div>"
+                    + "<a class=\"indexTable_sonHead\" href='ta.html'>" + users.author + "</a>" + "</div>"
+                    + "<div class=\"indexTableCon\">"
+                    + "<h1><a href='/lt_content?postId=" + users.postId + "'></a>" + users.title + "</h1>"
+                    // +"<p>"+postList[i].mBody+"</p>"
+                    + "<ul class=\"indexTable_conFoot clearfix\">"
+                    + "<li>" + users.pubTime + "</li>"
+                    + "<li><a href=\"lt_content.html\">评论</a>" + users.heat + "</li>"
+                    + "<li>最新回复时间：" +users.latestTime + "</li>"
+                    + "</ul>"
+                    + "</div>");
                 }
-                else{
-                    alert(postList.status);
-                }
-            }
+            },
             error:function err(){
-                alert("没有内容")
+                alert("没有内容")//可以弹
             }
-        }
     });
 }
-    $.ajax({
+$.ajax({
         type:"post",
         url : "/community/summaries",/*+postId + "/getPost"*/
         dataType:"json",
-        data:"indexs",
+        data:"",
         success:function succ(postList) {
-            for(let i=0;i<postList.length;i++){
-                if(postList.length>0){
-                    $("#postDiv").append("<div class='indexTable_son' style='display: block'>"+postList[i].postId
-                        +"<div class='inHeadPic'><img src="+postList[i].headPic+"alt=''></div>"
-                        +"<a class=\"indexTable_sonHead\" href='ta.html'>"+postList[i].author+"</a>"+"</div>"
-                        +"<div class=\"indexTableCon\">"
-                        +"<h1><a href='/lt_content?postId="+postList[i].postId+"'></a>"+postList[i].title+"</h1>"
+            for (let i = 0; i < postList.length; i++) {
+                if (postList.length > 0) {
+                    $("#postDiv").append("<div class='indexTable_son' style='display: block'>" + postList[i].postId
+                        + "<div class='inHeadPic'><img src=" + postList[i].headPic + "alt=''></div>"
+                        + "<a class=\"indexTable_sonHead\" href='ta.html'>" + postList[i].author + "</a>" + "</div>"
+                        + "<div class=\"indexTableCon\">"
+                        + "<h1><a href='/lt_content?postId=" + postList[i].postId + "'></a>" + postList[i].title + "</h1>"
                         // +"<p>"+postList[i].mBody+"</p>"
-                        +"<ul class=\"indexTable_conFoot clearfix\">"
-                        +"<li>"+postList[i].pubTime+"</li>"
-                        +"<li><a href=\"lt_content.html\">评论</a>"+postList[i].heat+"</li>"
-                        +"<li>最新回复时间："+postList[i].latestTime+"</li>"
-                        +"</ul>"
-                        +"</div>");
-                }
-                else{
+                        + "<ul class=\"indexTable_conFoot clearfix\">"
+                        + "<li>" + postList[i].pubTime + "</li>"
+                        + "<li><a href=\"lt_content.html\">评论</a>" + postList[i].heat + "</li>"
+                        + "<li>最新回复时间：" + postList[i].latestTime + "</li>"
+                        + "</ul>"
+                        + "</div>");
+                } else {
                     alert(postList.status);
                 }
             }
-        error:function err(){
-                alert("糟糕，出错了！")
-            }
-        }
+        },
+        error:alert("糟糕，出错了！"),//可以弹
     });
--- src/ELearning/assets/js/postContent.js
+++ src/ELearning/assets/js/postContent.js
@@ -1,51 +1,66 @@
-var url = window.location.href;
-var indexNum = url.lastIndexOf("\=");
-var postId=url.slice(indexNum+1);
-function initPost() {
-    var responseDiv=$("#responseDiv")
-    $.ajax({
+let url = window.location.href;
+let indexNum = url.lastIndexOf("\=");
+let postId=url.slice(indexNum+1);
+let responseDiv=$("#responseDiv").val();
+$.ajax({
         type: "post",
         url: "/community" + postId + "/getPost",
         dataType: "json",
-        data:"",
+        data:postId,
         success: function succ(response) {
             responseDiv.append("<div id='posts' class='lt_contentTop_Title'>"
                 + "<h1 class=\"lt_contentTop_Title\">" + response.title + "</h1>"
                 + "<div class='lt_contentTop_Msg'><a href='ta.html'>"
-                + "<div class='ltConPic'><img src="+response.headPic+" alt=''/></div>"
+                + "<div class='ltConPic'><img src=" + response.headPic + " alt=''/></div>"
                 + "<p class='indexTable_sonHead' href='ta.html'>" + response.author + "</p>"
                 + "</a>"
                 + "<p class='ltCon_detail'>" + response.body + "</p>"
                 + "<li>" + response.pubTime + "</li>"
                 // + "<li><button class='layui-btn layui-btn-xs' onclick='function liked() {response.like+=1; window.location.reload();}'>赞</button>" + response[i].like + "</li>"
-                +"<div id='commentDiv' class='ltCon_pend'>"
+                + "<div id='commentDiv' class='ltCon_pend'>"
                 + "<p class='ltCon_pendFont center'>评论区</p>"
-                +"</div>"
+                + "</div>"
                 + "</div>"
                 + "</div>");
-            let responses=response.resonses;
+            let responses = response.resonses;
             if (responses.length > 0) {
                 for (let i = 0; i < response.length; i++) {
                     responseDiv.append("<div class='ltCon_pendCen center'>"
-                        +"<div class='ltCon_pendCen_left' id=" + responses[i].resId + ">"
-                        + "<img src="+ responses[i].responderHeadPic + "alt=''/>"
+                        + "<div class='ltCon_pendCen_left' id=" + responses[i].resId + ">"
+                        + "<img src=" + responses[i].responderHeadPic + "alt=''/>"
                         + " <p class='ltCon_pendCen_right'>" + responses[i].responder + "</p>"
                         + "<span>" + responses[i].respondTime + "</span>"
-                        +"<span id='resp' onclick='sendComment()'>回复</span>"
+                        + "<span id='resp' onclick='sendComment()'>回复</span>"
                         + "<p class='ltReplay'>回复 <a href='/ta?Id=" + responses[i].pId + ">" + response.respondeer + "</a> " + response.content + "</p>"
-                        +"</div>"
+                        + "</div>"
                         + "</div>");
-                   /* function reply() {
-                       responseDiv.append("<input type='text'  value='@' id='comment' placeholder='加入讨论吧''>");
-                    }
-                    responseDiv.append( "<div class='ltDo'><span onclick='reply()'>回复</span></div>")*/
+                    /* function reply() {
+                        responseDiv.append("<input type='text'  value='@' id='comment' placeholder='加入讨论吧''>");
+                     }
+                     responseDiv.append( "<div class='ltDo'><span onclick='reply()'>回复</span></div>")*/
                 }
             } else {
                 alert(response.status);
             }
-            error:function err() {
-                alert("糟糕，出错了！")
-            }
-        }
+        },
+     error:alert("糟糕，出错了！"),
+    });
+let comment=$("#comment").val();
+let p=document.getElementById("resp");
+let pId=p.parentNode.id;//为什么给我报错？？？
+function sendComment(){
+    alert("111");
+    $.ajax({
+        type:"post",
+        url:"/community/respond",
+        dataType:"json",
+        data: "pId",//从哪儿获取
+            // "comment":comment,
+        success:function send(succ){
+            if(succ===1)
+            alert("发表成功！");
+            alert("发送失败！")
+        },
+        error:alert("发送失败，请重新发送！"),
     });
 }
